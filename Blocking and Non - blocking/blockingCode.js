d913ca53b5c4e751a2314bb808048eebb6e5346b
Anjudimpy <anjukr55555@gmail.com>
2024-12-16 15:32:47 +0530

    Blocking and non-blocking code

---
+++ Blocking and Non - blocking/blockingCode.js
@@ -0,0 +1,6 @@
+for (let i=0; i<=10000000000; i++){
+    //blocking
+}
+console.log("I wait for this long loop to finish");
+
+console.log("Hello");
---
+++ Blocking and Non - blocking/setTimeOut.js
@@ -0,0 +1,7 @@
+//Non- blocking code
+
+setTimeout(() =>{
+    console.log("I wait for this long loop to finish");
+
+});
+console.log("I am not wait");
